27864884ff07b6e6b5cf65bf93b9467429c7c12d
D45putspin <t14g0@protonmail.com>
2024-05-28 11:07:44 +0100

    f animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import EllipseShapeLandingOneBottomRight from "@/components/pages/landing-one/EllipseShapeLandingOneBottomRight";
 import EllipseShapeLandingOneTopLeft from "@/components/pages/landing-one/EllipseShapeLandingOneTopLeft";
 import Footer from "@/components/pages/landing-one/Footer";
@@ -5,20 +6,40 @@ import LandingOneBody from "@/components/pages/landing-one/LandingOneBody";
 import Navbar from "@/components/shared/Navbar";
 import Snowfall from "@/components/shared/Snowfall";
 import { Metadata } from "next";
+import { useState, useEffect } from "react";
 
-export const metadata: Metadata = {
-  title: "Home ",
 
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the width according to your definition of mobile
+    };
+
+    handleResize(); // Check on mount
+    window.addEventListener("resize", handleResize); // Add event listener
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Cleanup on unmount
+    };
+  }, []);
+
+  return isMobile;
 };
 
+
 const page = () => {
+  const isMobile = useIsMobile();
+
   return (
     <div className="position-relative overflow-hidden">
       <div className="container">
         <Navbar />
         <LandingOneBody />
         <Footer />
-        <Snowfall />
+        {!isMobile && <Snowfall />}
       </div>
       <EllipseShapeLandingOneTopLeft />
       <EllipseShapeLandingOneBottomRight />
